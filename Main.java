c98bffee725038bc02eb5972c12f0732a37c1f80
Evan <evanzhang1028@hotmail.com>
2019-05-13 14:12:02 -0400

    Create window

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        new Main().run();
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Climb to Grace");
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(800);
+        primaryStage.show();
     }
 }
