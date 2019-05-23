32c9039935388214415945cd8970efe671b84010
Evan <evanzhang1028@hotmail.com>
2019-05-22 21:09:30 -0400

    Finishing touches for the loading screen

--- src/LoadingScreen.java
+++ src/LoadingScreen.java
@@ -18,6 +18,7 @@ import javafx.util.*;
  * Revision history:
  * - May 13, 2019: Created ~Evan Zhang
  * - May 22, 2019: Finished ~Max Li
+ * - May 22, 2019: Finishing touches ~Evan Zhang
  * @version 1
  */
 public class LoadingScreen extends BaseScene {
@@ -35,29 +36,35 @@ public class LoadingScreen extends BaseScene {
 
         body.getChildren().add(imageView);
 
-        KeyFrame frame1 = new KeyFrame(Duration.seconds(0), new KeyValue(imageView.imageProperty(), ResourceLoader.loadImage("company-logo.png")), new KeyValue(imageView.opacityProperty(), 0.0));
-        KeyFrame fadein1 = new KeyFrame(Duration.seconds(4), new KeyValue(imageView.opacityProperty(), 1.0));
-        KeyFrame fadeout1 = new KeyFrame(Duration.seconds(6), new KeyValue(imageView.opacityProperty(), 0.0));
-        KeyFrame frame2 = new KeyFrame(Duration.seconds(6), new KeyValue(imageView.imageProperty(), ResourceLoader.loadImage("game-logo.png")));
-        KeyFrame fadein2 = new KeyFrame(Duration.seconds(10), new KeyValue(imageView.opacityProperty(), 1.0));
-        KeyFrame fadeout2 = new KeyFrame(Duration.seconds(12), new KeyValue(imageView.opacityProperty(), 0.0));
+        KeyFrame frame1 = new KeyFrame(Duration.seconds(0),
+                                       new KeyValue(imageView.imageProperty(),
+                                                    ResourceLoader.loadImage("company-logo.png")),
+                                                    new KeyValue(imageView.opacityProperty(), 0.0));
+        KeyFrame fadein1 = new KeyFrame(Duration.seconds(2), new KeyValue(imageView.opacityProperty(), 1.0));
+        KeyFrame fadeout1 = new KeyFrame(Duration.seconds(3), new KeyValue(imageView.opacityProperty(), 0.0));
+        KeyFrame frame2 = new KeyFrame(Duration.seconds(3), new KeyValue(imageView.imageProperty(),
+                                                                         ResourceLoader.loadImage("game-logo.png")));
+        KeyFrame fadein2 = new KeyFrame(Duration.seconds(5), new KeyValue(imageView.opacityProperty(), 1.0));
+        KeyFrame fadeout2 = new KeyFrame(Duration.seconds(6), new KeyValue(imageView.opacityProperty(), 0.0));
         Timeline timeline = new Timeline(frame1, fadein1, fadeout1, frame2, fadein2, fadeout2);
 
 
         for (int i = 1; i <= 6; i++)
-            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(10 + i * 2), new KeyValue(imageView.imageProperty(), ResourceLoader.loadImage("loadingscreen/house" + i + ".jpg")), new KeyValue(imageView.opacityProperty(), 1.0)));
+            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(5 + i),
+                                        new KeyValue(imageView.imageProperty(),
+                                                     ResourceLoader.loadImage("loadingscreen/house" + i + ".jpg")),
+                                                     new KeyValue(imageView.opacityProperty(), 1.0)));
 
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.setAutoReverse(true);
 
-        KeyFrame end = new KeyFrame(Duration.seconds(23), event -> game.updateState(State.MAIN_MENU));
+        KeyFrame end = new KeyFrame(Duration.seconds(12), event -> game.updateState(State.MAIN_MENU));
 
         timeline.getKeyFrames().add(end);
-
         timeline.play();
+
         root.setCenter(body);
 
         this.game.setScene(new Scene(root));
-        //this.game.updateState(State.MAIN_MENU);
     }
 }
