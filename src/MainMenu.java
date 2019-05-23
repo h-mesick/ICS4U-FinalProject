217df81d9497b56450f21cc9a21cff47b634ebac
h-mesick <lixiangm@gmail.com>
2019-05-22 22:17:40 -0400

    Main Menu Finished TODO: Level Select, High Scores

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,8 +1,12 @@
 import javafx.animation.*;
+import javafx.application.Platform;
 import javafx.event.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.canvas.*;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.scene.shape.*;
@@ -10,10 +14,11 @@ import javafx.scene.text.*;
 import javafx.stage.*;
 
 /**
- * @version 1
  * @author Evan Zhang
  * Revision history:
- *  - May 13, 2019: Created ~Evan Zhang
+ * - May 13, 2019: Created ~Evan Zhang
+ * - May 22, 2019: Finished - Max Li
+ * @version 1
  */
 public class MainMenu extends BaseScene {
     public MainMenu(Game game) {
@@ -21,15 +26,19 @@ public class MainMenu extends BaseScene {
     }
 
     public void initScene() {
-        VBox root = new VBox();
+        BorderPane root = new BorderPane();
 
-        Button btn = new Button("Next screen");
-        btn.setMinWidth(SCREEN_WIDTH / 2);
-        btn.setMinHeight(SCREEN_HEIGHT / 2);
-        btn.setOnAction(event -> {
-            this.game.updateState(State.LEVEL_SELECT);
-        });
-        root.getChildren().add(btn);
+        VBox body = new VBox(10);
+        body.setAlignment(Pos.CENTER);
+        body.getChildren().add(new ImageView(ResourceLoader.loadImage("game-logo.png")));
+        body.getChildren().add(getMainButton("Play Game", event -> game.updateState(State.LEVEL_SELECT)));
+        body.getChildren().add(getMainButton("How To Play", event -> game.updateState(State.TUTORIAL)));
+        body.getChildren().add(getMainButton("Help", event -> game.updateState(State.HELP)));
+        body.getChildren().add(getMainButton("High Scores", event -> game.updateState(State.HIGH_SCORES)));
+        body.getChildren().add(getMainButton("Quit", event -> Platform.exit()));
+
+        root.setCenter(body);
+
         this.game.setScene(new Scene(root));
     }
 }
