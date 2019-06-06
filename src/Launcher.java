6ce70548077df23f8ea6d2dac7df39060e1fee0c
Evan <evanzhang1028@hotmail.com>
2019-06-05 22:30:55 -0400

    Add launcher file to launch the game without importing JavaFX

---
+++ src/Launcher.java
@@ -0,0 +1,26 @@
+/**
+ * Evan Zhang and Max Li
+ * Mrs Krasteva
+ * Due: June 10, 2019
+ * The launcher class that launches the actual game so that JavaFX does not need to be imported.
+ */
+
+/**
+ * The launcher class that launches the actual game so that JavaFX does not need to be imported.
+ * <pre>
+ * Revision history:
+ *  - Jun 5, 2019: Created ~Evan Zhang
+ * </pre>
+ * @author Evan Zhang
+ * @version 1
+ */
+public class Launcher {
+    /**
+     * Calls the actual main method to begin the game.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
